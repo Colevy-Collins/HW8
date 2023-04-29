a260d1178e356230998bff04a437c4740fa3110a
Colevy Collins <colevy5111@icloud.com>
2023-04-29 09:26:39 -0400

    makepassword first try

--- src/makepassword.js
+++ src/makepassword.js
@@ -4,11 +4,30 @@ const {readFile, writeFile, hash} = require('./utility')
 
 function makepassword(passwordFileName, passwordEncFileName) {
 
-    ??? //test
+    function makepassword(passwordFileName, passwordEncFileName) {
+
+        let inputFromFile = readFile(passwordFileName);
+        let emailPasswordHashedPair
+
+        for( let i = 0; i < inputFromFile; i++){
+            let emailPasswordPair = inputFromFile[i].split(":")
+            emailPasswordPair[1] = hash(emailPasswordPair[1])
+
+            let email = emailPasswordPair[0]
+            let password = eamilPasswordPair[1]
+
+            emailPasswordPair[i] = email + ":" + password
+
+            emailPasswordHashedPair = emailPasswordPair
+        }
+
+        writeFile(emailPasswordHashedPair, "password.enc.txt")
+    }
 }
+
 
 if (require.main === module) {
     makepassword('./password.txt', './password.enc.txt')
 }
 
-module.exports = {???};
+module.exports = {makepassword};
